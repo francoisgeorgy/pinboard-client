df31b85d235f4b941c2dabfee23fb09d6a25ebbe
François Georgy <francois.georgy@gmail.com>
2017-12-05 16:56:11 +0100

    avoid warnings

--- backend/getbookmarks.php
+++ backend/getbookmarks.php
@@ -2,18 +2,19 @@
 
     header('Content-type: application/json');
     header('Cache-Control: no-cache');
+    header('Access-Control-Allow-Origin: *');   //TODO: restrict access to front-end only
 
     include 'lib/pinboard-api.php';
 
     $conf = parse_ini_file("conf.ini");
 
-    $t = $_GET["tags"];
+    $t = isset($_GET['tags']) ? $_GET['tags'] : null;
     $tags = empty($t) ? null : explode(',', $t);
 
-    $limit = $_GET["limit"];
+    $limit = isset($_GET['limit']) ? $_GET['limit'] : null;
     if (!is_numeric($limit)) $limit = empty($tags) ? 100 : 1000;
 
-    $force_refresh = $_GET["refresh"] == '1';
+    $force_refresh = isset($_GET['refresh']) ? $_GET['refresh'] == '1' : false;
 
     $cache_entry = empty($tags) ? '_' : preg_replace("/[^A-Za-z0-9_]/", '', trim(implode('_', $tags)));
     $cache_file = $conf['cache'].'/'.$cache_entry;
