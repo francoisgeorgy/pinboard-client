bc32170e8c4ebbba08558a41f4cb5d0878700bb1
François Georgy <francois.georgy@gmail.com>
2017-12-15 14:18:51 +0100

    added filter on bookmarks' titles

--- frontend/src/components/Bookmarks/index.jsx
+++ frontend/src/components/Bookmarks/index.jsx
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
+import SearchInput, {createFilter} from 'react-search-input'
+
+const KEYS_TO_FILTERS = ['tags', 'title'];
 
 class Bookmarks extends Component {
 
-    //TODO: add bookmark's tags as link's title attribute
+    constructor (props) {
+        super(props);
+        this.state = {
+            searchTerm: ''
+        };
+        this.searchUpdated = this.searchUpdated.bind(this)
+    }
+
+    searchUpdated (term) {
+        this.setState({searchTerm: term})
+    }
 
     render() {
         console.log('Bookmarks.render'/* , this.props */);
+
         if (this.props.bookmarks.length === 0) {
             return <div className={"bookmarks"}>select at least one tag</div>;
         } else {
+
+            const filtered = this.props.bookmarks.filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS));
+
             return (
-                <div className={"bookmarks"}>
-                    {
-                        this.props.bookmarks.map(item => (
-                            <div key={item.hash}><a href={item.url} target="_blank" title={item.tags.sort().join(' ')}>{item.title}</a></div>
-                        ))
-                    }
+                <div>
+                    <SearchInput className="search-input" placeholder="filter" onChange={this.searchUpdated} />
+                    <div className={"bookmarks"}>
+                        {
+                            filtered.map(item => (
+                                <div key={item.hash}><a href={item.url} target="_blank" title={item.tags.sort().join(' ')}>{item.title}</a></div>
+                            ))
+                        }
+                    </div>
                 </div>
             );
         }
